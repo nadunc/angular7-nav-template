b4855bb40b4bf5fcb384a87bbf2a3b437d2d7d2c
Nadun Chamikara <djnadunc@gmail.com>
2019-05-03 17:26:32 +0530

    main & side navs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SettingsComponent } from './settings/settings.component';
+import { ProfileComponent } from './settings/profile/profile.component';
+import { GeneralComponent } from './settings/general/general.component';
+import { SecurityComponent } from './settings/security/security.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+import { FriendsComponent } from './friends/friends.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'friends', component:FriendsComponent},
+  {
+    path : 'settings', 
+    component : SettingsComponent,
+    children : [
+      {path: 'profile', component:ProfileComponent},
+      {path: 'general', component:GeneralComponent},
+      {path: 'security', component:SecurityComponent},
+      {path: '**', redirectTo:'profile'}
+    ]
+  },
+  {
+    path : '**',
+    component : NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { SettingsComponent } from './settings/settings.component';
+import { ProfileComponent } from './settings/profile/profile.component';
+import { GeneralComponent } from './settings/general/general.component';
+import { SecurityComponent } from './settings/security/security.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+import { FriendsComponent } from './friends/friends.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent,
+    TopNavComponent,
+    SettingsComponent,
+    ProfileComponent,
+    GeneralComponent,
+    SecurityComponent,
+    NotFoundComponent,
+    HomeComponent,
+    FriendsComponent
   ],
   imports: [
     BrowserModule,
